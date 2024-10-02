18115bbc46db4f61faf51bd2ad098109786d0af2
calvin-doherty <cdoherty@hamilton.edu>
2024-10-02 17:25:37 +0000

    Description of Changes

--- Actvity3.R
+++ Actvity3.R
@@ -11,7 +11,7 @@ colnames(datCO2)[1] <- "Country"
 
 plot(datCO2$Year, datCO2$CO2, type = "l", xlab = "Year", ylab = "CO2 Emissions")
 
-#Prompt 1: Plot total all time emissions for the US, Mex, Can
+#Prompt 1: Plot air temp anomalies by hemisphere
 
 colnames(datCC)[1] <- "Hemisphere"
 
@@ -42,19 +42,68 @@ ggplot(dat_hemispheres, aes(x=bettertime, y=temperature_anomaly, col=Hemisphere)
 
 #Prompt 2: Plot total all time emissions for the US, Mex, Can
 
+NA_CO2 <- datCO2 %>%
+  filter(Country== "United States" | 
+           Country == "Mexico" | Country == "Canada")
+
 totalemissions <- NA_CO2 %>%
   group_by(Country) %>%
   summarize(TotalEmish = sum(CO2))
 
 ggplot(totalemissions)+
-  aes(x=Country, y=TotalEmish, col=Country)+
-  geom_col()
+  aes(x=Country, y=TotalEmish, fill=Country)+
+  geom_col()+
+  labs(x="Country", y="Total All Time CO₂ Emissions (tons)")
 
-NA_CO2 <- datCO2 %>%
-  filter(Country== "United States" | Country == "Mexico" | Country == "Canada")
+#Question 1: Make a graph that communicates about emissions of any countries
 
-ggplot(NA_CO2, aes(x=Year, y=CO2, col=Country))+
+Scandinavia_CO2 <- datCO2 %>%
+  filter(Country== "Norway" | 
+           Country == "Sweden" | Country == "Finland" | 
+           Country == "Denmark")
+
+ggplot(Scandinavia_CO2, aes(x=Year, y=CO2, col=Country))+
   geom_line()+
-  labs(x="Year", y= "CO2 emissions")+
+  labs(x="Year", y= "CO₂ emissions (tons)")+
   theme_classic()
+
+#Question 2: Make one graph for world air temp and one for CO2 emissions
+
+#Air Temp
+
+dat_world <- datCC %>%
+  filter(Hemisphere == "World")
+
+ggplot(dat_world, aes(x=bettertime, y=temperature_anomaly))+
+  geom_line()+
+  labs(x="Year", y= "Temperature anomaly (°C)")+
+  theme_classic()
+
+#CO2
+
+World_CO2 <- datCO2 %>%
+  filter(Country == "World")
+
+ggplot(World_CO2, aes(x=Year, y=CO2))+
+  geom_line()+
+  labs(x="Year", y= "CO₂ emissions (tons)")+
+  theme_classic()
+
+#Question 3: Download your own environmental data and create a visualization
+
+Brazil_Burned <- annual_burned_area_by_landcover %>%
+  filter(Entity == "Brazil")
+
+Brazil_Burned$Annual_Total <- 
+  rowSums(Brazil_Burned[, c("Yearly burned area across other land categories", 
+                            "Yearly burned area across croplands", 
+                            "Yearly burned area across forests",
+                            "Yearly burned area across savannas",
+                            "Yearly burned area across shrublands and grasslands") ])
+
+ggplot(Brazil_Burned, aes(x=Year, y=Annual_Total, col='red'))+
+  geom_line()+
+  labs(x="Year", y= "Annual Area Burned by Wildfires (ha)")+
+  theme_classic()+
+  theme(legend.position="none")
 
